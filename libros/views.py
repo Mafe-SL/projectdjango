b578ce330fd3d9c5e1dacfa8e443b82cf0c6b799
Mafe-SL <fernanda.mariastb@gmail.com>
2024-09-08 13:44:55 -0600

    Hola mundo - CRUD Basico

---
+++ libros/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Libro
+from .forms import LibroForm
+
+# Leer libros
+def listar_libros(request):
+    libros = Libro.objects.all()
+    return render(request, 'libros/listar_libros.html', {'libros': libros})
+
+# Crear libro
+def crear_libro(request):
+    if request.method == 'POST':
+        form = LibroForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_libros')
+    else:
+        form = LibroForm()
+    return render(request, 'libros/crear_libro.html', {'form': form})
+
+# Actualizar libro
+def actualizar_libro(request, id):
+    libro = get_object_or_404(Libro, id=id)
+    if request.method == 'POST':
+        form = LibroForm(request.POST, instance=libro)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_libros')
+    else:
+        form = LibroForm(instance=libro)
+    return render(request, 'libros/crear_libro.html', {'form': form})
+
+# Eliminar libro
+def eliminar_libro(request, id):
+    libro = get_object_or_404(Libro, id=id)
+    if request.method == 'POST':
+        libro.delete()
+        return redirect('listar_libros')
+    return render(request, 'libros/eliminar_libro.html', {'libro': libro})
+
